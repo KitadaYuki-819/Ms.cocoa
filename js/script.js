6ee74d086d345a48c1f86bc2b45bf861ae6b14e5
KitadaYuki-819 <kitada.0819@gmail.com>
2021-11-27 20:54:25 +0900

    11月27日

--- js/script.js
+++ js/script.js
@@ -1,12 +1,27 @@
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
 
-  var topBtn = $('.pagetop');
-  topBtn.hide();
+  // var topBtn = $('.pagetop');
+  // topBtn.hide();
 
+  // // ボタンの表示設定
+  // $(window).scroll(function () {
+  //   if ($(this).scrollTop() > 70) {
+  //     // 指定px以上のスクロールでボタンを表示
+  //     topBtn.fadeIn();
+  //   } else {
+  //     // 画面が指定pxより上ならボタンを非表示
+  //     topBtn.fadeOut();
+  //   }
+  // });
+
+
+  var topBtn = $('.top-page');
+  topBtn.hide();
+   var mv = $('.mv').height();
   // ボタンの表示設定
   $(window).scroll(function () {
-    if ($(this).scrollTop() > 70) {
+    if ($(this).scrollTop() > mv) {
       // 指定px以上のスクロールでボタンを表示
       topBtn.fadeIn();
     } else {
@@ -14,6 +29,8 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
       topBtn.fadeOut();
     }
   });
+
+
 
   // ボタンをクリックしたらスクロールして上に戻る
   topBtn.click(function () {
@@ -53,7 +70,7 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
 $(".qa-q").click(function(){
   $(this).next().slideToggle(300);
   $(this).css('border-radius', '10px 10px 10px 10px');
-  $(this).find('.qa-q__icon').toggleClass("is-open").300;
+  $(this).find('.qa-q__icon').toggleClass("is-open");
 });
 
 
@@ -67,3 +84,38 @@ $(".qa-q").click(function(){
 //       return false;
 //   });
 // });
+
+
+
+jQuery('.drawer-icon').on('click', function (e) {
+  e.preventDefault();
+  jQuery('.drawer-icon').toggleClass('is-active');
+  jQuery('.drawer-content').toggleClass('is-active');
+  jQuery('.drawer-background').toggleClass('is-active');
+  return false;
+});
+
+
+$(function () {
+  var imgHeight = $('.mv').outerHeight(); //画像の高さを取得。これがイベント発火位置になる。
+
+  var header = $('.header'); //ヘッダーコンテンツ
+
+  $(window).on('load scroll', function () {
+    if ($(window).scrollTop() < imgHeight) {
+      //メインビジュアル内にいるので、クラスを外す。
+      header.removeClass('headerColor-default');
+    } else {
+      //メインビジュアルより下までスクロールしたので、クラスを付けて色を変える
+      header.addClass('headerColor-default');
+    }
+  });
+});
+
+jQuery('.drawer-content__item a').on('click', function (e) {
+  e.preventDefault();
+  jQuery('.drawer-icon').toggleClass('is-active');
+  jQuery('.drawer-content').toggleClass('is-active');
+  jQuery('.drawer-background').toggleClass('is-active');
+  // return false;
+});
