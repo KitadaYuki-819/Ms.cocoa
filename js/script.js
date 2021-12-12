056c0ae4c4c121789b2fb117b8c32244372c16b3
KitadaYuki-819 <kitada.0819@gmail.com>
2021-12-12 10:30:12 +0900

    animationを作成しました。

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// const { arrayify } = require("browser-sync/dist/utils");
 
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
@@ -181,6 +182,18 @@ function langSet(argLang){
 		}
 	});
 });
+
+
+const targetElement = document.querySelectorAll(".animationTarget");
+console.log("画面の高さ", window.innerHeight)
+document.addEventListener("scroll", function() {
+  for(let i = 0; i < targetElement.length; i++) {
+    const getElementDistance = targetElement[i]. getBoundingClientRect().top + targetElement[i].clientHeight * .5
+    if(window.innerHeight > getElementDistance){
+      targetElement[i].classList.add("show");
+    }
+  }
+})
 
 
 
